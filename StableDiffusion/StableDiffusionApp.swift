ce3cd756b11cec8c2d9ab5b96376e81449569b07
Heical Chandra <haical31@gmail.com>
2024-05-31 09:00:26 +0700

    Initial commit

---
+++ StableDiffusion/StableDiffusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StableDiffusionApp.swift
+//  StableDiffusion
+//
+//  Created by Heical Chandra on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StableDiffusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
